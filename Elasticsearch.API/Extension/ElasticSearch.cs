c112cc5b267db2051ec542052da21e99c0c0a511
ISTINYE\erkan.salihoglu <erkan.salihoglu@istinye.edu.tr>
2023-06-05 16:28:48 +0300

    Add project files.

---
+++ Elasticsearch.API/Extension/ElasticSearch.cs
@@ -0,0 +1,17 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extension
+{
+    public  static class ElasticSearch
+    {
+        public static void AddElastic(this IServiceCollection service,IConfiguration configuration)
+        {
+            var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["URL"]!));
+            var settings = new ConnectionSettings(pool);
+            //settings.BasicAuthentication()
+            var client = new ElasticClient(settings);
+            service.AddSingleton(client);
+        }
+    }
+}
